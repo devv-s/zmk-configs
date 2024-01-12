5ccf65d614ad050d9f8ad19e65a40bf3d7d9de5c
Dev Vrat <itsdvvs.18@gmail.com>
2024-01-13 03:34:56 +0530

    home row mod implementation

--- config/helper.h
+++ config/helper.h
@@ -19,6 +19,7 @@
 #define ZMK_BEHAVIOR_CORE_macro_one_param compatible = "zmk,behavior-macro-one-param"; #binding-cells = <1>
 #define ZMK_BEHAVIOR_CORE_macro_two_param compatible = "zmk,behavior-macro-two-param"; #binding-cells = <2>
 #define ZMK_BEHAVIOR_CORE_mod_morph       compatible = "zmk,behavior-mod-morph";       #binding-cells = <0>
+#define ZMK_BEHAVIOR_CORE_mod_morph2      compatible = "zmk,behavior-mod-morph";       #binding-cells = <2>
 #define ZMK_BEHAVIOR_CORE_sticky_key      compatible = "zmk,behavior-sticky-key";      #binding-cells = <1>
 #define ZMK_BEHAVIOR_CORE_tap_dance       compatible = "zmk,behavior-tap-dance";       #binding-cells = <0>
 #define ZMK_BEHAVIOR_CORE_tri_state       compatible = "zmk,behavior-tri-state";       #binding-cells = <0>
