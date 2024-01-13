cbaff32541ab4a4eb59c93997ee7ad5a4313bc05
Dev Vrat <itsdvvs.18@gmail.com>
2024-01-13 17:38:00 +0530

    removing conflicting symbols

--- config/mysymbols.h
+++ config/mysymbols.h
@@ -1,4 +1,4 @@
-#pragma once;
+#pragma once
 
 #define ALPH 0
 #define NUMB 1
@@ -9,71 +9,71 @@
 #define LHRM 6
 #define RHRM 7
 
-#define [Q] &kp Q
-#define [W] &kp W
-#define [E] &kp E
-#define [R] &kp R
-#define [T] &kp T
-#define [Y] &kp Y
-#define [U] &kp U
-#define [I] &kp I
-#define [O] &kp O
-#define [P] &kp P
-#define [A] &kp A
-#define [S] &kp S
-#define [D] &kp D
-#define [F] &kp F
-#define [G] &kp G
-#define [H] &kp H
-#define [J] &kp J
-#define [K] &kp K
-#define [L] &kp L
-#define [:] &kp SEMI
-#define [Z] &kp Z
-#define [X] &kp X
-#define [C] &kp C
-#define [V] &kp V
-#define [B] &kp B
-#define [N] &kp N
-#define [M] &kp M
-#define [,] &kp COMMA
-#define [.] &kp DOT
-#define [/] &kp FSLH
-#define [LSF] &kp LSHFT
-#define [RSF] &kp RSHFT
+#define _Q_ &kp Q
+#define _W_ &kp W
+#define _E_ &kp E
+#define _R_ &kp R
+#define _T_ &kp T
+#define _Y_ &kp Y
+#define _U_ &kp U
+#define _I_ &kp I
+#define _O_ &kp O
+#define _P_ &kp P
+#define _A_ &kp A
+#define _S_ &kp S
+#define _D_ &kp D
+#define _F_ &kp F
+#define _G_ &kp G
+#define _H_ &kp H
+#define _J_ &kp J
+#define _K_ &kp K
+#define _L_ &kp L
+#define _:_ &kp SEMI
+#define _Z_ &kp Z
+#define _X_ &kp X
+#define _C_ &kp C
+#define _V_ &kp V
+#define _B_ &kp B
+#define _N_ &kp N
+#define _M_ &kp M
+#define _,_ &kp COMMA
+#define _._ &kp DOT
+#define _/_ &kp FSLH
+#define LSF &kp LSHFT
+#define RSF &kp RSHFT
 
-#define [1] &kp N1
-#define [2] &kp N2
-#define [3] &kp N3
-#define [4] &kp N4
-#define [5] &kp N5
-#define [6] &kp N6
-#define [7] &kp N7
-#define [8] &kp N8
-#define [9] &kp N9
-#define [0] &kp N0
+#define _1_ &kp N1
+#define _2_ &kp N2
+#define _3_ &kp N3
+#define _4_ &kp N4
+#define _5_ &kp N5
+#define _6_ &kp N6
+#define _7_ &kp N7
+#define _8_ &kp N8
+#define _9_ &kp N9
+#define _0_ &kp N0
 
-#define [`] &kp GRAVE
-#define [!] &kp EXCL
-#define [@] &kp AT
-#define [#] &kp HASH
-#define [$] &kp DLLR
-#define [%] &kp PRCNT
-#define [^] &kp CARET
-#define [&] &kp AMPS
-#define [*] &kp ASTRK
+#define _`_ &kp GRAVE
+#define _!_ &kp EXCL
+#define _@_ &kp AT
+#define _#_ &kp HASH
+#define _$_ &kp DLLR
+#define _%_ &kp PRCNT
+#define _^_ &kp CARET
+#define _&_ &kp AMPS
+#define _*_ &kp ASTRK
 #define LPR &kp LPAR
 #define RPR &kp RPAR
 #define LBK &kp LBKT
 #define RBK &kp RBKT
-#define [=] &kp EQUAL
-#define ['] &kp SQT
-#define [\] &kp BSLH
+#define _=_ &kp EQUAL
+#define _'_ &kp SQT
+#define _\_ &kp BSLH
 
-#define [←] &kp LEFT
-#define [↑] &kp UP
-#define [→] &kp RIGHT
-#define [↓] &kp DOWN
+#define ← &kp LEFT
+#define ↑ &kp UP
+#define → &kp RIGHT
+#define ↓ &kp DOWN
 
 #define LAT &kp LALT
 #define LCR &kp LCTRL
@@ -83,19 +83,17 @@
 #define RCR &kp RCTRL
 #define RCM &kp RCMD
 #define RSF &kp RSHFT
-#define OPR &kp OPER
-#define OUT &kp OUT
 
 #define ... &trans
 #define ... &trans
 #define ___ &none
 
-#define BKSP+ &qlt SYST BSPC
-#define TAB+  &lt  SMBL TAB
-#define ESC+  &lt  NUMB ESC
-#define OPR+  &lt  LHRM &none
+#define BKSP_ &qlt SYST BSPC
+#define TAB_  &lt  SMBL TAB
+#define ESC_  &lt  NUMB ESC
+#define LHM   &lt  LHRM &none
 
-#define RET+ &lt  NAVG RET
-#define DEL+ &qlt SYST DEL
-#define OUT+ &lt  RHRM &none
+#define RET_ &lt  NAVG RET
+#define DEL_ &qlt SYST DEL
+#define RHM  &lt  RHRM &none
 
