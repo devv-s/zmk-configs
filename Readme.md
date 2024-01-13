0f5f540ea83d005f7dd88f76cb5d652a458b1777
Dev Vrat <itsdvvs.18@gmail.com>
2024-01-14 01:18:01 +0530

    Decluttering keymaps file.
    - placed function keys on the left side.
    - availed modifier buttons on symbols layer.
    - availed shift buttons on all layers.

--- Readme.md
+++ Readme.md
@@ -4,7 +4,8 @@ My personal keymaps for corne keyboard which use zmk as its firmware.
 
 ## To Do
 - [ ] Map mouse buttons.
-- [ ] Fix Home Row mods.
+- [x] Fix Home Row mods.
 - [ ] Turn on the RGB if on System Layer for indication.
-- [x] Tap and hold behavior for keys like backspace and space and del if possible.
+- [ ] Tap and hold behavior for keys like backspace and space and del if possible.
+- [ ] Place brightness and volume controls and some layer.
 
---
+++ config/keynames.h
@@ -0,0 +1,46 @@
+#pragma once
+
+#define ALPH 0
+#define NUMB 1
+#define SMBL 2
+#define NAVG 3
+#define FUNC 4
+#define SYST 5
+#define LHRM 6
+#define RHRM 7
+
+#define GRV GRAVE
+#define XCL EXCL
+#define HSH HASH
+#define DLR DLLR
+#define PCT PRCNT
+#define CRT CARET
+#define AMP AMPS
+#define AST ASTRK
+#define LPR LPAR
+#define RPR RPAR
+#define LBK LBKT
+#define RBK RBKT
+#define EQU EQUAL
+#define FSL FSLH
+#define BSL BSLH
+#define SMI SEMI
+#define CMM COMMA
+
+#define LFT LEFT
+#define RIT RIGHT
+#define DWN DOWN
+
+#define LAT LALT
+#define LCT LCTRL
+#define LCM LCMD
+#define LSF LSHFT
+#define RAT RALT
+#define RCT RCTRL
+#define RCM RCMD
+#define RSF RSHFT
+#define BSP BSPC
+#define BLK SPACE
+
+#define _x_ &none
+
