2e55b4269d05c3dc19e30369d4eca334cd9f45d9
Dev Vrat <itsdvvs.18@gmail.com>
2024-01-13 18:25:41 +0530

    resolving symbols

--- config/mysymbols.h
+++ config/mysymbols.h
@@ -9,91 +9,50 @@
 #define LHRM 6
 #define RHRM 7
 
-#define _Q_ &kp Q
-#define _W_ &kp W
-#define _E_ &kp E
-#define _R_ &kp R
-#define _T_ &kp T
-#define _Y_ &kp Y
-#define _U_ &kp U
-#define _I_ &kp I
-#define _O_ &kp O
-#define _P_ &kp P
-#define _A_ &kp A
-#define _S_ &kp S
-#define _D_ &kp D
-#define _F_ &kp F
-#define _G_ &kp G
-#define _H_ &kp H
-#define _J_ &kp J
-#define _K_ &kp K
-#define _L_ &kp L
-#define _:_ &kp SEMI
-#define _Z_ &kp Z
-#define _X_ &kp X
-#define _C_ &kp C
-#define _V_ &kp V
-#define _B_ &kp B
-#define _N_ &kp N
-#define _M_ &kp M
-#define _,_ &kp COMMA
-#define _._ &kp DOT
-#define _/_ &kp FSLH
-#define LSF &kp LSHFT
-#define RSF &kp RSHFT
+#define GRV GRAVE
+#define XCL EXCL
+#define HSH HASH
+#define DLR DLLR
+#define PCT PRCNT
+#define CRT CARET
+#define AMP AMPS
+#define AST ASTRK
+#define LPR LPAR
+#define RPR RPAR
+#define LBK LBKT
+#define RBK RBKT
+#define EQL EQUAL
+#define FSL FSLH
+#define BSL BSLH
+#define SMI SEMI
+#define CMM COMMA
 
-#define _1_ &kp N1
-#define _2_ &kp N2
-#define _3_ &kp N3
-#define _4_ &kp N4
-#define _5_ &kp N5
-#define _6_ &kp N6
-#define _7_ &kp N7
-#define _8_ &kp N8
-#define _9_ &kp N9
-#define _0_ &kp N0
+#define ← LEFT
+#define ↑ UP
+#define → RIGHT
+#define ↓ DOWN
 
-#define _`_ &kp GRAVE
-#define _!_ &kp EXCL
-#define _@_ &kp AT
-#define _#_ &kp HASH
-#define _$_ &kp DLLR
-#define _%_ &kp PRCNT
-#define _^_ &kp CARET
-#define _&_ &kp AMPS
-#define _*_ &kp ASTRK
-#define LPR &kp LPAR
-#define RPR &kp RPAR
-#define LBK &kp LBKT
-#define RBK &kp RBKT
-#define _=_ &kp EQUAL
-#define _'_ &kp SQT
-#define _\_ &kp BSLH
-
-#define ← &kp LEFT
-#define ↑ &kp UP
-#define → &kp RIGHT
-#define ↓ &kp DOWN
-
-#define LAT &kp LALT
-#define LCR &kp LCTRL
-#define LCM &kp LCMD
-#define LSF &kp LSHFT
-#define RAT &kp RALT
-#define RCR &kp RCTRL
-#define RCM &kp RCMD
-#define RSF &kp RSHFT
+#define LAT LALT
+#define LCR LCTRL
+#define LCM LCMD
+#define LSF LSHFT
+#define RAT RALT
+#define RCR RCTRL
+#define RCM RCMD
+#define RSF RSHFT
+#define BSP BSPC
+#define SPC SPACE
 
 #define ... &trans
 #define ... &trans
 #define ___ &none
 
-#define BKSP_ &qlt SYST BSPC
-#define TAB_  &lt  SMBL TAB
-#define ESC_  &lt  NUMB ESC
-#define LHM   &lt  LHRM &none
+#define BKSPs SYST BSPC
+#define TABs  SMBL TAB
+#define ESCs  NUMB ESC
+#define LHM   LHRM &none
 
-#define RET_ &lt  NAVG RET
-#define DEL_ &qlt SYST DEL
-#define RHM  &lt  RHRM &none
+#define RETs NAVG RET
+#define DELs SYST DEL
+#define RHM  RHRM &none
 
