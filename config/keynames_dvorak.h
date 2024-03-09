5f20fff3e81fbf897103b511b6b48fe7a2c6c5a9
Dev Vrat Singh <itsdvvs.18@gmail.com>
2024-03-10 01:21:18 +0530

    aliases for symbol key

---
+++ config/keynames_dvorak.h
@@ -0,0 +1,22 @@
+// ALIASES THAT MUST ONLY BE USED IN SYMBOL LAYER.
+// SYNTAX: #DEFINE DVORAK_KEY QWERTY_KEY.
+
+// Number Row (before BACKSPACE).
+#define DLBK MINUS
+#define DRBK EQUAL
+
+// Top Alphabet Row (after L).
+#define DFSL LBKT
+#define DEQU RBKT
+#define DBSL BSLH
+
+// Middle Alphabet Row
+#define DMNS SQT
+
+// Bottom Alphabet Row
+#define DSMI Z
+#define DCMM W
+#define DSQT Q
+#define DDOT E
+
+
